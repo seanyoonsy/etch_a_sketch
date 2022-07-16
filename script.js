f9c401549a099082300d2fda747cc247105a9e59
Sean Yoon <seanyoonsy@gmail.com>
2022-07-16 02:28:10 -0700

    Add defer to script link in HTML, tidy up script.js and add createGrid(); line, add styling to .cell

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
 
+createGrid();
+
 function createGrid() {
     makeRows(16);
     makeColumns(16);
@@ -13,14 +15,14 @@ function makeRows(rowNum) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
     };
-};
+}
 
 function makeColumns(cellNum) {
+    
     for (i = 0; i < rows.length; i++) {
         for (j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
-
     };
-};
+}
