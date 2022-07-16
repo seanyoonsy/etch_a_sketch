46b299031ab6123b0e8f9ada0ee74b55af166f36
Sean Yoon <seanyoonsy@gmail.com>
2022-07-16 00:31:37 -0700

    Create makeRows function that uses a loop to create a specified number of rows in the grid

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const container = document.getElementById("container");
 
+function makeRows(rowNum) {
+
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
 
