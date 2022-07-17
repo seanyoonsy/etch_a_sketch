cae6562e396efb330b44bd665ce3f688da1d37d8
Sean Yoon <seanyoonsy@gmail.com>
2022-07-17 00:07:49 -0700

    Create setupGrid function that will create columns and rows, and has a loop with event listeners for cells to change color

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById('sizeSlider');
 const grid = document.getElementById('grid');
 
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+
 function setCurrentMode(newMode) {
     activateButton(newMode);
     currentMode = newMode;
 }
 
-colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+function setupGrid(size) {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', changeColor);
+        gridElement.addEventListener('mousedown', changeColor);
+        grid.appendChild(gridElement);
+    }
+}
