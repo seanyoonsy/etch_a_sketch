4cb8c24e9e8b3c78ffadfcd0226312c6f326e86a
Sean Yoon <seanyoonsy@gmail.com>
2022-07-16 23:52:25 -0700

    Create function setCurrentMode(newMode) and add oninput event for the color picking tool

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const colorPicker = document.getElementById('colorPicker')
-const eraserBtn = document.getElementById('eraserBtn')
-const clearBtn = document.getElementById('clearBtn')
-const sizeValue = document.getElementById('sizeValue')
-const sizeSlider = document.getElementById('sizeSlider')
-const grid = document.getElementById('grid')
+const colorPicker = document.getElementById('colorPicker');
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const grid = document.getElementById('grid');
+
+function setCurrentMode(newMode) {
+    activateButton(newMode);
+    currentMode = newMode;
+}
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
